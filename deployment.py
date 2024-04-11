11b5e48852d140a36dbb9e4cc75793ed8af85668
ruskstoic <145134275+ruskstoic@users.noreply.github.com>
2024-04-11 13:26:12 +0800

    Update deployment.py
    Added import and streamlit title

--- deployment.py
+++ deployment.py
@@ -1,1 +1,30 @@
+## Import
+import streamlit
+import pandas
+import numpy
+import tensorflow
+import requests
+from streamlit_gsheets import GSheetsConnection
+import streamlit_analytics
+from streamlit.runtime import get_instance
+from streamlit.runtime.scriptrunner import get_script_run_ctx
+from streamlit.runtime.scriptrunner.script_run_context import get_script_run_ctx
+from streamlit_cookies_manager import EncryptedCookieManager
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaIoBaseDownload
+from google.auth.credentials import Credentials
+from google.oauth2 import service_account
 
+## Streamlit Tracker Start
+streamlit_analytics.start_tracking()
+
+## Streamlit Interface
+st.title('Shall we generate a never before seen manga synopsis?')
+
+
+
+## Streamlit Tracker End
+streamlit_analytics.stop_tracking(unsafe_password='test123')
