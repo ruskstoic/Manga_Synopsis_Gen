5f104a97a1a0bb52dac34b9a9446ff8b2836837c
ruskstoic <145134275+ruskstoic@users.noreply.github.com>
2024-04-14 14:49:31 +0800

    Update deployment.py
    Added generate button:
    Load model from google drive
    Save model into temp file
    Load model into tensorflow
    Generate text function
    Generate text and print for user
    Save data to google sheet

--- deployment.py
+++ deployment.py
@@ -6,7 +6,6 @@ import numpy as np
 import uuid
 import base64
 from streamlit_cookies_manager import EncryptedCookieManager
-import tensorflow as tf
 import requests
 from streamlit_gsheets import GSheetsConnection
 import streamlit_analytics
@@ -22,6 +21,8 @@ from google.auth.credentials import Credentials
 from google.oauth2 import service_account
 from datetime import datetime, timedelta
 import pytz
+import tensorflow as tf
+
 
 ## Streamlit Tracker Start
 streamlit_analytics.start_tracking()
@@ -46,13 +47,15 @@ def get_or_create_tab_ID():
   return st.session_state.tab_id
 
 #Function to compile user info
-def log_user_info():
+def log_user_info(user_name, user_id, formatted_datetime, tab_id, seed_text, gen_text, temperature, num_gen_words):
   user_info = {'Name': user_name,
                'User_ID': user_id,
                'Datetime_Entered': formatted_datetime,
                'Tab_ID': tab_id,
                'Seed_Text': seed_text,
-               'Gen_Text': gen_text
+               'Gen_Text': gen_text,
+               'Temp': temperature,
+               'Num_Gen_Words': num_gen_words
               }
   df_log_entry = pd.DataFrame([user_info])
   return df_log_entry
@@ -86,11 +89,89 @@ if user_name:
   formatted_datetime = datetime_entered.strftime(datetime_format)
 
   # User Input Seed Text, Temperature, Num_Gen_Words
-  seed_text = st.text_area('Input some text here and we will generate a synopsis from this!\n\n')
-  temperature = st.slider('Choose the Temperature You Would Like (The higher the temperature, the more random the generated words)', 0.3, 1.0)
-  num_gen_words = st.slider('Choose the Number of Generated Words You Would Like', 20, 60)
+  seed_text = str(st.text_area('Input some text here and we will generate a synopsis from this!\n\n'))
+  temperature = float(st.slider('Choose the Temperature You Would Like (The higher the temperature, the more random the generated words)', 0.3, 1.0))
+  num_gen_words = int(st.slider('Choose the Number of Generated Words You Would Like', 20, 60))
 
   st.write(seed_text, temperature, num_gen_words)
+
+  ## User has input seed text and click generate button
+  if seed_text is not None and st.button('Generate'):
+
+    #Get Model from Google Drive
+    st.write('Generating...')
+    model_id = ''
+    request = service.files().get_media(fileId=model_id)
+    fh = io.BytesIO()
+    downloader = request.execute()
+    fh.write(downloader)
+    fh.seek(0)
+
+    #Save Model to a Temporary File
+    temp_file_path = '/tmp/model_name.h5'
+    with open(temp_file_path, 'wb') as f:
+      f.write(fh.read())
+
+    #Load Model on Tensorflow
+    model = tf.keras.models.load_model(temp_file_path)
+    st.success('Model file loaded successfully!')
+
+    #Simple Generator Function
+    def generate_text(model, tokenizer, seq_len, seed_text, num_gen_words, temperature):
+      output_text = []
+      input_text = seed_text
+      prev_pred_word_idx = 0
+  
+      for i in range(num_gen_words-1):
+        encoded_text = tokenizer_second.texts_to_sequences([input_text])[0]
+        pad_encoded = pad_sequences([encoded_text], maxlen=seq_len, truncating='pre', dtype='float32') #expects a list of sequences
+        pad_encoded = np.array(pad_encoded)
+        pred_distribution = model.predict(pad_encoded, verbose=0)[0]
+    
+        #Temperature parameter
+        new_pred_distribution = np.power(pred_distribution, (1/temperature))
+        new_pred_distribution[prev_pred_word_idx] = 0 #prevents previous word from being next word
+        new_pred_distribution = new_pred_distribution / new_pred_distribution.sum()
+    
+        #Choose word with highest probability as next word
+        choices = range(new_pred_distribution.size)
+        pred_word_idx = np.random.choice(a=choices, p=new_pred_distribution) #randomly chooses word
+        # pred_word_idx = np.argmax(new_pred_distribution) #choose max probability word
+        prev_pred_word_idx = pred_word_idx
+        pred_word = tokenizer.index_word.get(pred_word_idx, '')
+        input_text += ' ' + pred_word
+        output_text.append(pred_word)
+    
+        #Create Index-Word-Probability Dataframe for Data Visualisation
+        if i==num_gen_words - 1:
+          chosen = pd.DataFrame({'Probability': new_pred_distribution})
+          chosen['Word'] = chosen.index.map(lambda x: tokenizer.index_word.get(x, ''))
+          chosen = chosen[['Word', 'Probability']]
+          chosen_top3 = chosen.nlargest(3, 'Probability')
+          chosen_bottom3 = chosen.nsmallest(3, 'Probability')
+          st.write('Top 3 Probabilities:', chosen_top3) ###comment out print for pipeline
+          st.write('Bottom 3 Probabilities:', chosen_bottom3) ###comment out print for pipeline
+      return ' '.join(output_text)
+
+    #Generate Synopsis and Show Result
+    filter_size = 10 #changeable parameter
+    gen_text = generate_text(model = model,
+                                tokenizer = tokenizer_second,
+                                seq_len = filter_size - 1, # why does -1 work also
+                                seed_text = seed_text,
+                                num_gen_words = num_gen_words,
+                                temperature = temperature)
+    st.write(f'Seed Text: {seed_text}\n\nModel Generation: {gen_text}...')
+
+    ## Save Data to Google Sheet
+    log_entry_df = log_user_info(user_name=user_name, user_id=user_id, formatted_datetime=formatted_datetime, tab_id=tab_id, seed_text=seed_text, gen_text=gen_text, temperature=temperature,
+                                 num_gen_words=num_gen_words):
+    conn = st.connection('gsheets', type=GSheetsConnection)
+    existing_data = conn.read(worksheet='Sheet2', usecols=[0,1,2,3,4,5,6,7], end='A')
+    existing_df = pd.DataFrame(existing_data, columns=['Name', 'User_ID', 'Datetime_Entered', 'Tab_ID', 'Seed_Text', 'Gen_Text', 'Temp', 'Num_Gen_Words'])
+    combined_df = pd.concat([existing_df, log_entry_df], ignore_index=True)
+    conn.update(worksheet='Sheet2', data=combined_df)
+    st.cache_data.clear()
 
 ## Streamlit Tracker End
 streamlit_analytics.stop_tracking(unsafe_password)
