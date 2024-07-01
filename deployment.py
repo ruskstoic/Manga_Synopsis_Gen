8242ee1980af5bb54ed4e57c794aa0bce653b6aa
ruskstoic <145134275+ruskstoic@users.noreply.github.com>
2024-07-01 13:46:20 +0800

    Update deployment.py
    Comment out use legacy keras and all import keras

--- deployment.py
+++ deployment.py
@@ -1,6 +1,6 @@
 import os
 # # Set the environment variable to use legacy Keras
-os.environ["TF_USE_LEGACY_KERAS"] = "1"
+# os.environ["TF_USE_LEGACY_KERAS"] = "1"
 
 ## Import
 import sys
@@ -26,7 +26,7 @@ from google.oauth2 import service_account
 from datetime import datetime, timedelta
 import pytz
 import tensorflow as tf
-import tf_keras as keras
+# import tf_keras as keras
 # import keras
 from transformers import GPT2Tokenizer, TFGPT2LMHeadModel
 import pickle
